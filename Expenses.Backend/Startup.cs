fc9242fda07a9e728e0b608b26330574dd9c70f5
Marcos Escudero <marcosescudero@hotmail.com>
2018-10-06 02:25:10 -0300

    MVC Backend & API Presentations

---
+++ Expenses.Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Expenses.Backend.Startup))]
+namespace Expenses.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
